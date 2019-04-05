665a12a2b9d8cb7ee29fc905e1b5738e40fba1c0
joshduf <JDuFault@stanford.edu>
2019-04-05 13:04:08 -0700

    Delete 2016_preprocessModel.py

--- 2016_preprocessModel.py
+++
@@ -1,8 +0,0 @@
-# Creates and saves numpy vectors for model.
-# Warning: this takes a full day to run.
-
-import processData
-
-processData.createEvaluateArrays(PATHS = ["./csvs/2016_partial_ht_content.csv"],  ID_INDEX = 0,
-                                 TEXT_INDEX = 1, Y_INDEX = 2, MAX_FIELD = 2000,
-                                 X_SAVE = "./model/2016_x.npy", Y_SAVE = "./model/2016_y.npy", ID_SAVE = "./model/2016_ids.csv")
